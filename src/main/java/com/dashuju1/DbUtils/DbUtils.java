b517f31874b6dc1bdcf7582ae6f3ba9f1f9f352d
Lutengyu <lutengyu@outlook.com>
2022-03-25 14:38:43 +0800

    提交数据库连接池文件

---
+++ src/main/java/com/dashuju1/DbUtils/DbUtils.java
@@ -0,0 +1,58 @@
+package com.dashuju1.DbUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
+
+public class DbUtils {
+    private static Properties properties = new Properties();
+    private static DataSource dataSource;
+
+    static {
+        try {
+            FileInputStream is = new FileInputStream("config/dbcp.properties");
+            properties.load(is);
+            dataSource = BasicDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void close(ResultSet rs, Statement stat, Connection conn){
+        try {
+            if(rs!=null){
+                rs.close();
+            }
+            if(stat!=null){
+                stat.close();
+            }
+            if(conn!=null){
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
