193b70978d0350422688a2c0b5966246c8e68eec
Lutengyu <lutengyu@outlook.com>
2022-03-27 02:49:22 +0800

    修改分页查询代码（未完成）

--- src/main/java/com/dashuju1/service/PageServlet2.java
+++ src/main/java/com/dashuju1/service/PageServlet2.java
@@ -4,17 +4,46 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet(name = "PageServlet2", urlPatterns = "/PageServlet2")
 public class PageServlet2 extends HttpServlet {
     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        System.out.println("111");
+//        Map<String, String[]> parameterMap = request.getParameterMap();
+//        String[] s1 = parameterMap.get("a");
+//        String[] s2 = parameterMap.get("n");
+//        String a = s1[0];
+//        int n = Integer.parseInt(s2[0]);
+//        System.out.println(a);
+//        System.out.println(n);
+//        if (a.equals("a")){
+//            if (n>1){
+//                n = n-1;
+//                request.setAttribute("n", n);
+//                request.getRequestDispatcher("/PageServlet").forward(request, response);
+//            }
+//        }else if (a.equals("b")){
+//            n = n+1;
+//            request.setAttribute("n", n);
+//            request.getRequestDispatcher("/PageServlet").forward(request, response);
+//        }else {
+//            n=Integer.parseInt(a);
+//            request.setAttribute("n", n);
+//            request.getRequestDispatcher("/PageServlet").forward(request, response);
+//        }
+////        doGet(request,response);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int u = Integer.parseInt(request.getParameter("up"));
-        request.setAttribute("s",u);
-        int g= Integer.parseInt(request.getParameter("go"));
-        request.setAttribute("s",g);
-        int d= Integer.parseInt(request.getParameter("down"));
-        request.setAttribute("s",d);
-        request.getRequestDispatcher("/PageServlet").forward(request, response);
+        int n = Integer.parseInt(request.getParameter("go"));
+        if (n>=1) {
+            request.setAttribute("n", n);
+            request.getRequestDispatcher("/PageServlet").forward(request, response);
+        }
+
     }
 }
+
